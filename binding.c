7933fb43082505d69144c47ac29e7910b0ae9771
Balakrishna Avulapati <ba@bavulapati.com>
2025-12-07 12:03:47 +0530

    Complete TODOs
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- binding.c
+++ binding.c
@@ -8,41 +8,6 @@
 #include <string.h>
 #include <utf.h>
 
-// #define MEMORY_DEBUG
-#ifdef MEMORY_DEBUG
-
-void *
-debug_mem_malloc(size_t size, const char *func, uint line) {
-  void *memory = malloc(size);
-  printf("%15s memory %p of size %6zu at line:%5u in func:%s\n", "allocating", memory, size, line, func);
-  return memory;
-}
-
-void *
-debug_mem_realloc(void *memory, size_t size, const char *func, uint line) {
-  void *temp = realloc(memory, size);
-  printf("%15s memory %p of size %6zu at line:%5u in func:%s\n", "re-allocating", temp, size, line, func);
-  return temp;
-}
-void
-debug_mem_free(void *memory, size_t size, const char *func, uint line) {
-  printf("%15s memory %p of size %6zu at line:%5u in func:%s\n", "freeing", memory, size, line, func);
-  return free(memory);
-}
-
-#define malloc(n) \
-  debug_mem_malloc(n, __func__, __LINE__) /* Replaces malloc. \
-                                           */
-#define realloc(n, m) \
-  debug_mem_realloc(n, m, __func__, __LINE__)               /* Replaces realloc. */
-#define free(n, m) debug_mem_free(n, m, __func__, __LINE__) /* Replaces free. */
-
-#else
-
-#define free(n, m) free(n)
-
-#endif
-
 typedef struct {
   js_env_t *env;
   js_deferred_t *deferred;
@@ -54,29 +19,39 @@ reject_promise(state_t *state, utf8_t *str) {
   js_value_t *resolution;
   js_value_t *message;
   js_handle_scope_t *handle_scope;
+  int err = 0;
 
-  js_open_handle_scope(state->env, &handle_scope);
-  js_create_string_utf8(state->env, str, -1, &message);
-  js_create_error(state->env, NULL, message, &resolution);
-  js_reject_deferred(state->env, state->deferred, resolution);
-  js_close_handle_scope(state->env, handle_scope);
+  err = js_open_handle_scope(state->env, &handle_scope);
+  assert(err == 0);
+  err = js_create_string_utf8(state->env, str, -1, &message);
+  assert(err == 0);
+  err = js_create_error(state->env, NULL, message, &resolution);
+  assert(err == 0);
+  err = js_reject_deferred(state->env, state->deferred, resolution);
+  assert(err == 0);
+  err = js_close_handle_scope(state->env, handle_scope);
+  assert(err == 0);
 }
 
 void
 resolve_promise(state_t *state) {
   js_value_t *resolution;
   js_handle_scope_t *handle_scope;
+  int err = 0;
 
-  js_open_handle_scope(state->env, &handle_scope);
-  // TODO: add assert
-  js_create_string_utf8(state->env, (void *) state->buf.base, state->buf.len, &resolution);
-  js_resolve_deferred(state->env, state->deferred, resolution);
-  js_close_handle_scope(state->env, handle_scope);
+  err = js_open_handle_scope(state->env, &handle_scope);
+  assert(err == 0);
+  err = js_create_string_utf8(state->env, (void *) state->buf.base, state->buf.len, &resolution);
+  assert(err == 0);
+  err = js_resolve_deferred(state->env, state->deferred, resolution);
+  assert(err == 0);
+  err = js_close_handle_scope(state->env, handle_scope);
+  assert(err == 0);
 }
 
 void
 close_cb(uv_handle_t *handle) {
-  free(handle, sizeof(*(uv_tcp_t *) handle));
+  free(handle);
 }
 
 void
@@ -95,8 +70,8 @@ read_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
       reject_promise(state, (utf8_t *) uv_strerror(nread));
       uv_close((uv_handle_t *) stream, close_cb);
     }
-    free(state->buf.base, state->buf.len);
-    free(state, sizeof(*state));
+    free(state->buf.base);
+    free(state);
   } else if (nread > 0) {
     state->buf.base = realloc(state->buf.base, nread + state->buf.len);
     memcpy(state->buf.base + state->buf.len, buf->base, nread);
@@ -104,7 +79,7 @@ read_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
   }
 
   if (buf != NULL) {
-    free(buf->base, buf->len);
+    free(buf->base);
   }
 }
 
@@ -113,13 +88,13 @@ write_cb(uv_write_t *req, int status) {
   if (status < 0) {
     state_t *state = req->handle->data;
     reject_promise(state, (void *) uv_strerror(status));
-    free(state, sizeof(*state));
+    free(state);
     uv_close((void *) req->handle, close_cb);
   }
   uv_buf_t *buf = req->data;
-  free(buf->base, buf->len);
-  free(buf, sizeof(*buf));
-  free(req, sizeof(*req));
+  free(buf->base);
+  free(buf);
+  free(req);
 }
 
 void
@@ -143,19 +118,19 @@ connect_cb(uv_connect_t *req, int status) {
   w_req->data = buf;
   status = uv_write(w_req, req->handle, buf, 1, write_cb);
   if (status != 0) {
-    free(buf, sizeof(*buf));
-    free(w_req, sizeof(*w_req));
+    free(buf);
+    free(w_req);
     goto cleanup;
   }
-  free(req, sizeof(*req));
+  free(req);
   return;
 
 cleanup:
   uv_close(handle, close_cb);
   reject_promise(state, (utf8_t *) uv_strerror(status));
-  free(req->data, strlen(req->data) + 1);
-  free(req, sizeof(*req));
-  free(state, sizeof(*state));
+  free(req->data);
+  free(req);
+  free(state);
   return;
 }
 
@@ -172,117 +147,82 @@ bare_addon_tcp_connect(js_env_t *env, js_callback_info_t *info) {
   size_t argc = 3;
   js_value_t *argv[3];
   err = js_get_callback_info(env, info, &argc, argv, NULL, NULL);
-  if (err != 0) {
-    goto cleanup;
-  }
+  assert(err == 0);
 
   if (argc < 3) {
-    js_throw_type_error(env, NULL, "Wrong number of arguments. Expects 3 arguments.");
+    err = js_throw_type_error(env, NULL, "Wrong number of arguments. Expects 3 arguments.");
+    assert(err == 0);
     goto cleanup;
   }
 
   js_value_type_t value_type;
   err = js_typeof(env, argv[0], &value_type);
-  if (err != 0) {
-    goto cleanup;
-  }
+  assert(err == 0);
 
   if (value_type != js_string) {
-    js_throw_type_error(env, NULL, "Wrong type of arguments. The host must be a string.");
+    err = js_throw_type_error(env, NULL, "Wrong type of arguments. The host must be a string.");
+    assert(err == 0);
     goto cleanup;
   }
   err = js_typeof(env, argv[1], &value_type);
-  if (err != 0) {
-    goto cleanup;
-  }
+  assert(err == 0);
 
   if (value_type != js_number) {
-    js_throw_type_error(env, NULL, "Wrong type of arguments. The port must be a number.");
+    err = js_throw_type_error(env, NULL, "Wrong type of arguments. The port must be a number.");
+    assert(err == 0);
     goto cleanup;
   }
 
   err = js_typeof(env, argv[2], &value_type);
-  if (err != 0) {
-    goto cleanup;
-  }
+  assert(err == 0);
   if (value_type != js_string) {
-    js_throw_type_error(env, NULL, "Wrong type of arguments. The messeage must be a string.");
+    err = js_throw_type_error(env, NULL, "Wrong type of arguments. The messeage must be a string.");
+    assert(err == 0);
     goto cleanup;
   }
 
   size_t host_len = 0;
 
   err = js_get_value_string_utf8(env, argv[0], NULL, 0, &host_len);
-  if (err != 0) {
-    goto cleanup;
-  }
+  assert(err == 0);
 
   host = malloc(host_len + 1);
-  // TODO: Nuke the NULL check of memory
-  if (host == NULL) {
-    js_throw_error(env, NULL, "Error allocating memory");
-    goto cleanup;
-  }
-  // TODO: verify if memset is necessary by reading the bare code
-  memset(host, 'G', host_len);
-  host[host_len] = '\0';
   err = js_get_value_string_utf8(env, argv[0], (utf8_t *) host, host_len + 1, NULL);
-  if (err != 0) {
-    goto cleanup;
-  }
+  assert(err == 0);
 
   uint32_t port = 0;
   err = js_get_value_uint32(env, argv[1], &port);
-  if (err != 0) {
-    goto cleanup;
-  }
+  assert(err == 0);
 
   struct sockaddr_in addr;
   err = uv_ip4_addr(host, port, &addr);
   if (err != 0) {
-    js_throw_error(env, NULL, uv_strerror(err));
+    err = js_throw_error(env, NULL, uv_strerror(err));
+    assert(err == 0);
     goto cleanup;
   }
 
-  // TODO: free host as early as possible like this
-  free(host, host_len + 1);
+  free(host);
   host = NULL;
   size_t msg_len;
 
   err = js_get_value_string_utf8(env, argv[2], NULL, 0, &msg_len);
-  if (err != 0) {
-    goto cleanup;
-  }
+  assert(err == 0);
 
   msg = malloc(msg_len + 1);
-  if (msg == NULL) {
-    js_throw_error(env, NULL, "Error allocating memory");
-    goto cleanup;
-  }
-  memset(msg, 'G', msg_len);
-  msg[msg_len] = '\0';
   err = js_get_value_string_utf8(env, argv[2], (utf8_t *) msg, msg_len + 1, NULL);
-  if (err != 0) {
-    goto cleanup;
-  }
+  assert(err == 0);
 
   handle = malloc(sizeof(*handle));
-  if (handle == NULL) {
-    js_throw_error(env, NULL, "Error allocating memory");
-    goto cleanup;
-  }
 
   err = uv_tcp_init(uv_default_loop(), handle);
   if (err != 0) {
-    js_throw_error(env, NULL, uv_strerror(err));
+    err = js_throw_error(env, NULL, uv_strerror(err));
+    assert(err == 0);
     goto cleanup;
   }
 
   req = malloc(sizeof(*req));
-  if (req == NULL) {
-    js_throw_error(env, NULL, "Error allocating memory");
-    goto cleanup;
-  }
   req->data = msg;
   err = uv_tcp_connect(req, handle, (struct sockaddr *) &addr, connect_cb);
   if (err != 0) {
@@ -292,37 +232,31 @@ bare_addon_tcp_connect(js_env_t *env, js_callback_info_t *info) {
   }
 
   state = malloc(sizeof(*state));
-  if (state == NULL) {
-    js_throw_error(env, NULL, "Error allocating memory");
-    goto cleanup;
-  }
-  // TODO: just set buf.len to 0 instead of memset
-  // *state = {.buf = {.len = 0}}; // TODO: verify if default init works
-  memset(state, 0, sizeof(*state));
+  state->buf.base = NULL;
+  state->buf.len = 0;
   state->env = env;
   handle->data = state;
 
   err = js_create_promise(env, &state->deferred, &promise);
   assert(err == 0);
-  // TODO: check if libjs API failures are practical.
 
   return promise;
 
 cleanup:
   if (state != NULL) {
-    free(state, sizeof(*state));
+    free(state);
   }
   if (req != NULL) {
-    free(req, sizeof(*req));
+    free(req);
   }
   if (handle != NULL) {
-    free(handle, sizeof(*handle));
+    free(handle);
   }
   if (msg != NULL) {
-    free(msg, msg_len + 1);
+    free(msg);
   }
   if (host != NULL) {
-    free(host, host_len + 1);
+    free(host);
   }
   return promise;
 }
